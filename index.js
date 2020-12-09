bb813de63bf94f744984ee0d57177ec8f028b2c1
Richard Hillman <Richard.CC.Hillman@gmail.com>
2020-12-09 15:42:17 -0800

    finished crud and routes for one model

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const express = require('express');
 require('dotenv').config();
-const ModelS = require('./lib/utils/tesla/model_s');
+const express = require('express');
 const app = express();
+const ModelS = require('./lib/utils/tesla/model_s');
 app.use(express.json());
 
 // ---------------------------------------------
 
-app.get('/', (req, res) => {
+app.get('/tesla/model_s', (req, res) => {
   ModelS
     .find()
     .then(modelS => res.send(modelS));
@@ -15,11 +15,35 @@ app.get('/', (req, res) => {
 
 // -------------------------------------------
 
-app.post('/', async(req, res) => {
-  const modelS = await ModelS.insert(req.body);
-  res.send(modelS);
+app.get('/tesla/model_s/:id', (req/res) => {
+  ModelS
+    .findById(req.params.id)
+    .then(modelS => res.send(modelS));
+});
+
+// -------------------------------------------
+
+app.post('/tesla/model_s', async(req, res) => {
+  modelS 
+    .insert(req.body)
+    .then(modelS => res.send(modelS));
 });
 
 // ---------------------------------------------
+
+app.put('/tesla/model_s/:id', (req, res) => {
+  modelS
+    .update(req.params.id, req.body)
+    .then(modelS => res.send(modelS));
+});
+
+// ----------------------------------------------
+
+app.delete('/tesla/model_s/:id', (req, res) => {
+  modelS
+    .delete(req.params.id)
+    .then(modelS => res.send(modelS));
+}); 
+
 
 module.exports = { app };
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 require('./lib/utils/pool').connect();
-
 const { app } = require('./index');
 const PORT = 3001;
+
 
 app.listen(PORT, () => {
     console.log(`Started on ${PORT}`);
