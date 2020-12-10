386b77a5ce33a1e60909b5e1ea5ff879628b4c99
Richard Hillman <Richard.CC.Hillman@gmail.com>
2020-12-10 14:28:58 -0800

    delete route is working and all tests are passing

--- __tests__/model_s.test.js
+++ __tests__/model_s.test.js
@@ -110,6 +110,27 @@ describe('endpoint', () => {
 
   //   -----------------------------------------------------
 
-});
+  test('delete a car', async() => {
+    const expectation = {
+      title: 'Model S',
+      descript: 'an evolution in automobile engineering. Dual Motor Model S is a categorical improvement on conventional all-wheel drive systems. With two motors, one in the front and one in the rear, Model S digitally and independently controls torque to the front and rear wheels.', 
+      model: 'Long Range Plus',
+      color: 'Solid Black',
+      wheelType: '19inch Tempest',
+      interior: 'All Black',
+      id:'1'
+    };
 
+    const data = await request(app)
+      .delete('/tesla/model_s/1')
+      .expect('Content-Type', /json/)
+      .expect(200);
+
+    expect(data.body).toEqual(expectation);
+
+  });
+
+  // END=======================================================
+
+});
 
